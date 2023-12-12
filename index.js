52b675369c1ecb637220294a9fcf087aca8d9e0f
Thomas Grivet <contact@thomasgrivet.fr>
2023-12-12 17:02:47 +0100

    fix: new files structure

---
+++ index.js
@@ -0,0 +1,5 @@
+import { registerRootComponent } from 'expo';
+
+import { Application } from './src/components/Application/Application';
+
+registerRootComponent(Application);
---
+++ src/components/Application/Application.tsx
@@ -0,0 +1,13 @@
+import { SafeAreaView, Text } from 'react-native';
+
+export const Application = () => (
+    <SafeAreaView
+        style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}
+    >
+        <Text>{'Application'}</Text>
+    </SafeAreaView>
+);
